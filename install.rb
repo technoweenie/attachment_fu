bd18f3711b63d3ca1e86020dd4827052572d56bb
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-12-23 20:04:45 +0000

    fix install.rb snafu
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/attachment_fu@2579 567b1171-46fb-0310-a4c9-b4bef9110e78

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 require 'fileutils'
 
 s3_config = File.dirname(__FILE__) + '/../../../config/amazon_s3.yml'
-FileUtils.cp File.dirname(__FILE__) + '/amazon_s3.yml.tpl', s3config unless File.exist?(s3_config)
+FileUtils.cp File.dirname(__FILE__) + '/amazon_s3.yml.tpl', s3_config unless File.exist?(s3_config)
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
