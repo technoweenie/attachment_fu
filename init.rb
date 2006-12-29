8e8f0779ef0bcf2e29dfe094e5ae9b890050e9c7
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-12-29 09:15:31 +0000

    Object.const_defined? that is
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/attachment_fu@2619 567b1171-46fb-0310-a4c9-b4bef9110e78

--- init.rb
+++ init.rb
@@ -9,5 +9,5 @@ Tempfile.class_eval do
 end
 
 ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
-Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
+Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
 FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
