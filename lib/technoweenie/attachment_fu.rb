4f9d2be0ad3014f2374871d8e765a082f751e450
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-12-29 09:13:12 +0000

    handle stringio uploaded files
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/attachment_fu@2618 567b1171-46fb-0310-a4c9-b4bef9110e78

--- lib/technoweenie/attachment_fu.rb
+++ lib/technoweenie/attachment_fu.rb
@@ -239,7 +239,12 @@ module Technoweenie # :nodoc:
         return nil if file_data.nil? || file_data.size == 0 
         self.content_type = file_data.content_type
         self.filename     = file_data.original_filename if respond_to?(:filename)
-        self.temp_path    = file_data.path
+        if file_data.is_a?(StringIO)
+          file_data.rewind
+          self.temp_data = file_data.read
+        else
+          self.temp_path    = file_data.path
+        end
       end
 
       # Gets the latest temp path from the collection of temp paths.  While working with an attachment,
