79110e972dbd4a92dcffc55bd48185d7263e1965
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-07-19 20:06:09 +0000

    recognize image/jpg as an image mime type
    git-svn-id: http://svn.techno-weenie.net/projects/plugins/attachment_fu@2933 567b1171-46fb-0310-a4c9-b4bef9110e78

--- lib/technoweenie/attachment_fu.rb
+++ lib/technoweenie/attachment_fu.rb
@@ -2,7 +2,7 @@ module Technoweenie # :nodoc:
   module AttachmentFu # :nodoc:
     @@default_processors = %w(ImageScience Rmagick MiniMagick)
     @@tempfile_path      = File.join(RAILS_ROOT, 'tmp', 'attachment_fu')
-    @@content_types      = ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png']
+    @@content_types      = ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg']
     mattr_reader :content_types, :tempfile_path, :default_processors
     mattr_writer :tempfile_path
 
