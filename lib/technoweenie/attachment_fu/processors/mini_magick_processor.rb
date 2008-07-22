3a4ce6f10511bf005ebec41ef40c30e6d9ff5ddf
Ryan Sonnek <ryan@socialcast.com>
2008-07-22 13:37:52 -0500

    restore old version to fix resizing images to crop correctly

--- lib/technoweenie/attachment_fu/processors/mini_magick_processor.rb
+++ lib/technoweenie/attachment_fu/processors/mini_magick_processor.rb
@@ -38,22 +38,38 @@ module Technoweenie # :nodoc:
         # Performs the actual resizing operation for a thumbnail
         def resize_image(img, size)
           size = size.first if size.is_a?(Array) && size.length == 1
-          img.combine_options do |commands|
-            commands.strip unless attachment_options[:keep_profile]
-            if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))
-              if size.is_a?(Fixnum)
-                size = [size, size]
-                commands.resize(size.join('x'))
-              else
-                commands.resize(size.join('x') + '!')
-              end
+          if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))
+            if size.is_a?(Fixnum)
+              size = [size, size]
+              img.resize(size.join('x'))
             else
-              commands.resize(size.to_s)
+              img.resize(size.join('x') + '!')
             end
+          else
+            n_size = size.gsub(/!/,'').split("x").map(&:to_i)
+            if size.ends_with? "!"
+              aspect = n_size[0].to_f / n_size[1].to_f
+              ih, iw = img[:height], img[:width]
+              w, h = (ih * aspect), (iw / aspect)
+              w = [iw, w].min.to_i
+              h = [ih, h].min.to_i
+              if ih > h
+                shave_off =  ((ih - h) / 2).round
+                img.shave("0x#{shave_off}")
+              end
+              if iw > w
+                shave_off = ((iw - w ) / 2).round
+                img.shave("#{shave_off}x0")
+              end
+              img.resize(size.to_s)
+            else
+              img.resize(size.to_s)
+            end
+            self.temp_path = img
           end
-          self.temp_path = img
         end
       end
     end
   end
 end
+
