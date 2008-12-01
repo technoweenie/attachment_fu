28ff8e24a8906a6585662a947fa79057d24dc9ba
rick <technoweenie@gmail.com>
2008-11-30 20:44:10 -0800

    ensure that the thumbnail associations are created on the correct class

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-Dependencies.load_once_paths << File.expand_path(File.join(lib_path, '..', 'vendor'))
+(ActiveSupport.const_defined?(:Dependencies) ? ActiveSupport::Dependencies : Dependencies).load_once_paths << File.expand_path(File.join(lib_path, '..', 'vendor'))
 
 config.after_initialize do
   AttachmentFu.setup ActiveRecord::Base
