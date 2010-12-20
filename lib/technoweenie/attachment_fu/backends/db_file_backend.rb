60edb0e2878ea4ff1b2de101c9dda4d2511d70cb
Ben Osheroff <ben@zendesk.com>
2010-12-20 14:33:11 -0800

    unprotect methods

--- lib/technoweenie/attachment_fu/backends/db_file_backend.rb
+++ lib/technoweenie/attachment_fu/backends/db_file_backend.rb
@@ -2,11 +2,22 @@ module Technoweenie # :nodoc:
   module AttachmentFu # :nodoc:
     module Backends
       # Methods for DB backed attachments
-      module DbFileBackend
-        def self.included(base) #:nodoc:
+      class DbFileBackend < Delegator
+        def initialize(obj, opts)
+          @obj = obj
+          @attachment_options = opts
+        end
+   
+        def self.included_in_base(base)
           Object.const_set(:DbFile, Class.new(ActiveRecord::Base)) unless Object.const_defined?(:DbFile)
           base.belongs_to  :db_file, :class_name => '::DbFile', :foreign_key => 'db_file_id'
         end
+ 
+        def __getobj__
+          @obj
+        end
+
+        def attachment_options; @attachment_options; end
 
         # Creates a temp file with the current db data.
         def create_temp_file
@@ -18,21 +29,20 @@ module Technoweenie # :nodoc:
           db_file.data
         end
         
-        protected
-          # Destroys the file.  Called in the after_destroy callback
-          def destroy_file
-            db_file.destroy if db_file
+        # Destroys the file.  Called in the after_destroy callback
+        def destroy_file
+          db_file.destroy if db_file
+        end
+        
+        # Saves the data to the DbFile model
+        def save_to_storage
+          if save_attachment?
+            (db_file || build_db_file).data = temp_data
+            db_file.save!
+            @obj.class.update_all ['db_file_id = ?', obj.db_file_id = db_file.id], ['id = ?', id]
           end
-          
-          # Saves the data to the DbFile model
-          def save_to_storage
-            if save_attachment?
-              (db_file || build_db_file).data = temp_data
-              db_file.save!
-              self.class.update_all ['db_file_id = ?', self.db_file_id = db_file.id], ['id = ?', id]
-            end
-            true
-          end
+          true
+        end
       end
     end
   end
