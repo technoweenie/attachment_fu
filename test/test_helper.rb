8c69ffbd56927699b139b53a9175f8e47ab882f4
Ben Osheroff <ben@gimbo.net>
2011-02-01 11:41:08 -0800

    require mocha framework

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,7 @@ require 'test/unit'
 require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
 require 'active_record/fixtures'
 require 'action_controller/test_process'
+require 'mocha'
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
