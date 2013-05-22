30a7109754f7afa2b87277ec367a75d11f4e5411
Jais Cheema <jais@trikeapps.com>
2013-05-22 10:12:28 +1000

    Add in compatiblity for ruby19 for tmp file name generation

--- init.rb
+++ init.rb
@@ -4,6 +4,9 @@ Tempfile.class_eval do
   # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
   def make_tmpname(basename, n)
     ext = nil
+    # the n parameter is optional in ruby 1.9 so accomodate for that
+    # http://apidock.com/ruby/Dir/Tmpname/make_tmpname
+    n = "" unless n
     sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
   end
 end
