9ebc7c1942ad64526fd48cb77254ffc709136cf5
Wesley Moore <wes@trikeapps.com>
2016-01-15 14:27:29 +1100

    Restore colorspace handling of ImageMagic pre-6.7.x
    http://www.imagemagick.org/discourse-server/viewtopic.php?f=2&t=20501
    
    [#111744838]

--- lib/technoweenie/attachment_fu/processors/mini_magick_processor.rb
+++ lib/technoweenie/attachment_fu/processors/mini_magick_processor.rb
@@ -53,21 +53,21 @@ module Technoweenie # :nodoc:
                 # Different config depending on whether the image is being enlarged or shrunk
                 if dimensions.max < size
                   # Upsample - "LanczosSharp-11.5"
-                  # convert $< -colorspace RGB +sigmoidal-contrast 11.5 -filter LanczosSharp -distort Resize 630x630 -sigmoidal-contrast 11.5 -colorspace sRGB $@
-                  commands.colorspace 'RGB'
+                  # convert $< -colorspace sRGB +sigmoidal-contrast 11.5 -filter LanczosSharp -distort Resize 630x630 -sigmoidal-contrast 11.5 -colorspace RGB $@
+                  commands.colorspace 'sRGB'
                   commands.sigmoidal_contrast + '11.5'
                   commands.filter 'LanczosSharp'
                   commands.distort "Resize", [size, size].join('x')
                   commands.sigmoidal_contrast '11.5'
-                  commands.colorspace 'sRGB'
+                  commands.colorspace 'RGB'
                 else
                   # Downsample "Lanczos3Sharpest"
-                  # convert $< -colorspace RGB -filter Lanczos -define filter:blur=0.88549061701764 -distort Resize 630x630  -colorspace sRGB $@
-                  commands.colorspace 'RGB'
+                  # convert $< -colorspace sRGB -filter Lanczos -define filter:blur=0.88549061701764 -distort Resize 630x630  -colorspace RGB $@
+                  commands.colorspace 'sRGB'
                   commands.filter 'Lanczos'
                   commands.define 'filter:blur=0.88549061701764'
                   commands.distort "Resize", [size, size].join('x')
-                  commands.colorspace 'sRGB'
+                  commands.colorspace 'RGB'
                 end
               else
                 commands.resize(size.join('x') + '!')
